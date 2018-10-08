c1fd8c482be7bacdc7939e81d970277b27ae6696
D7703_18 <d7703_18@d7703-18ui-imac.local>
2018-10-08 14:44:18 +0900

    10/08

---
+++ Book_Parsing/BookData.swift
@@ -0,0 +1,13 @@
+//
+//  BookData.swift
+//  Book_Parsing
+//
+//  Created by D7703_18 on 2018. 10. 2..
+//  Copyright © 2018년 201550057. All rights reserved.
+//
+
+import Foundation
+class BookData {
+    var title = ""
+    var author = ""
+}
